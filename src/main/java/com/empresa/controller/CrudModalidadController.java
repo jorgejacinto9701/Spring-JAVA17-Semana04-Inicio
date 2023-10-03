ffe717a7d7f4aac7b5bdb12ceee1efe027b60b26
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-10-02 19:11:10 -0500

    justes

--- src/main/java/com/empresa/controller/CrudModalidadController.java
+++ src/main/java/com/empresa/controller/CrudModalidadController.java
@@ -1,13 +1,13 @@
 package com.empresa.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CrudModalidadController {
 
 
-	@RequestMapping("/verCrudModalidad")
+	@GetMapping("/verCrudModalidad")
 	public String verInicio() {
 		return "crudModalidad";
 	}
